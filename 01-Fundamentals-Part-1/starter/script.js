cab20c9c0dd21b53ac9ce85f050d2dc383eae1f1
liuduwei <17341730328@163.com>
2022-08-21 23:05:25 +0800

    start guess gameOC

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,34 @@
+
+console.log(123+11);
+
+let firstname = "liu";
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+let cao;
+console.log(cao);
+
+true;
+console.log(true);
+
+let javascriptIsFun = true; 
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+
+
+const brityYear = 2000;
+
+var job = 'programmer'
+
+console.log (2 * 2, 5 / 10, 2 ** 3);
+
+
+const test = `I'm ${firstname} , a`;
+const newLine = `I'm 
+liu
+du
+wei`
+console.log(test)
+console.log(newLine)
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".number").textContent = 12;
+document.querySelector(".guess").value = 10;
+document.querySelector(".message").textContent = 'congrutulations ��';
