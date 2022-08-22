7b1bb689c074a3883446e7dff8f60c1192cf2597
liuduwei <17341730328@163.com>
2022-08-22 20:02:17 +0800

    start guess number game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,19 @@
 'use strict';
 
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
 
-document.querySelector(".number").textContent = 12;
-document.querySelector(".guess").value = 10;
-document.querySelector(".message").textContent = 'congrutulations ��';
+// document.querySelector(".number").textContent = 12;
+// document.querySelector(".guess").value = 10;
+// document.querySelector(".message").textContent = 'congrutulations ��';
+
+const secretNumber = Math.trunc((Math.random() * 20));
+let guess;
+
+console.log(Number(document.querySelector('.guess').value));
+
+document.querySelector('.check').addEventListener('click',
+function () {
+guess = Number(document.querySelector('.guess').value);
+});
+
+
